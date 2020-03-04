68d8b9155867535701046b5e8dde6f2937074095
ovejorgensen <ove.kristian.jorgensen@gmail.com>
2020-03-04 09:59:16 +0100

    first commit

---
+++ libs/gradientScript.js
@@ -0,0 +1,49 @@
+console.log("SNAKK TIL MEG");
+
+window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
+
+viewer.setEDLEnabled(true);
+viewer.setFOV(60);
+viewer.setPointBudget(1 * 1000 * 1000);
+viewer.setBackground("gradient"); // ["skybox", "gradient", "black", "white"];
+viewer.setMinNodeSize(10);
+viewer.loadSettingsFromURL();
+
+// viewer.loadGUI(() => {
+//     viewer.setLanguage('en');
+//     $("#menu_appearance").next().show();
+//     $("#menu_tools").next().show();
+//     $("#menu_scene").next().show();
+//     gulpviewer.toggleSidebar();
+// });
+
+viewer.scene.view.position.set(1441.04, -826.93, 1604.68);
+viewer.scene.view.lookAt(new THREE.Vector3(296.27, -162.42, 786.24));
+
+let radius = 520;
+let gradientName = "TURBO";
+let gradient = Potree.Gradients[gradientName];
+
+let x = 0;
+let y = 400
+
+Potree.loadPointCloud("myData/pointclouds/nuPage/cloud.js", "nuPage", e => {
+    let pointcloud = e.pointcloud;
+    let material = pointcloud.material;
+    viewer.scene.addPointCloud(pointcloud);
+    material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
+
+    material.activeAttributeName = "elevation";
+    material.gradient = gradient;
+
+    e.pointcloud.position.set(x, y, e.pointcloud.position.z);
+    material.elevationRange = [150, 225];//magic number
+
+    e.pointcloud.updateMatrixWorld();
+    let box = e.pointcloud.pcoGeometry.tightBoundingBox.clone();
+    box.applyMatrix4(e.pointcloud.matrixWorld);
+    let center = box.getCenter();
+    material.shape = Potree.PointShape.CIRCLE;
+    material.size = 1;
+    viewer.fitToScreen();
+});
---
+++ myData/pointclouds/nuPage/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 64223546,
+    "boundingBox": {
+        "lx": 284170.717,
+        "ly": 5206487.353,
+        "lz": -192.71,
+        "ux": 285730.011,
+        "uy": 5208046.647,
+        "uz": 1366.5839999999944
+    },
+    "tightBoundingBox": {
+        "lx": 284170.717,
+        "ly": 5206487.353,
+        "lz": -192.71,
+        "ux": 285730.011,
+        "uy": 5207770.306,
+        "uz": 280.981
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 13.50388240814209,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
