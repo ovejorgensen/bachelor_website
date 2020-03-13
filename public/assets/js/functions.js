c348b1e4b2c6c3808b0f4770101483bd15b5a081
Tor-Fredrik <tori.torgersen@hotmail.com>
2020-03-13 17:17:00 +0100

    annotest

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,7 +1,11 @@
+
 document.getElementById('btn1').onclick=function(){ 
     if(viewer.getBackground() == "gradient") viewer.setBackground("skybox") ;
     else viewer.setBackground("gradient");
 }
 
+document.getElementById('btn7').onclick=function(){
+    
+}
 
 
--- public/potree/libs/gradientScript.js
+++ public/potree/libs/gradientScript.js
@@ -24,11 +24,43 @@ let gradient = Potree.Gradients[gradientName];
 let x = 0;
 let y = 400
 
+
 Potree.loadPointCloud("potree/myData/pointclouds/nuPage/cloud.js", "nuPage", e => {    
     let pointcloud = e.pointcloud;
     let material = pointcloud.material;
     viewer.scene.addPointCloud(pointcloud);
     material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
+
+    {
+        {
+            let elTitle = $(`
+                <span>
+                    About Annotations
+                    <img src="${Potree.resourcePath}/icons/goto.svg" 
+                        name="action_set_scene"
+                        class="annotation-action-icon" 
+                        style="filter: invert(1);" />
+                </span>
+            `);
+            elTitle.find("img[name=action_set_scene]").click( (event) => {
+                event.stopPropagation();
+                viewer.setScene(viewer.scene); 
+            });
+            elTitle.toString = () => "About Annotations";
+
+            let aAbout1 = new Potree.Annotation({
+                position: [285093.891, 5207463.696, 400.305],
+                title: elTitle,
+                cameraPosition: [285153.891, 5207513.696, 450.305],
+                cameraTarget: [285093.891, 5207463.696, 400.305],
+                description: `<ul><li>Click on the annotation label to move a predefined view.</li> 
+                <li>Click on the icon to execute the specified action.</li>
+                In this case, the action will bring you to another scene and point cloud.</ul>`
+            });
+
+            viewer.scene.annotations.add(aAbout1);
+        }
+    }
 
     material.activeAttributeName = "elevation";
     material.gradient = gradient;
@@ -43,4 +75,5 @@ Potree.loadPointCloud("potree/myData/pointclouds/nuPage/cloud.js", "nuPage", e =
     material.shape = Potree.PointShape.CIRCLE;
     material.size = 1;
     viewer.fitToScreen();
+
 });
