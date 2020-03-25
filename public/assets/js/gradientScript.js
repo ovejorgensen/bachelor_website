add6e6d0d3823900b5ff768f9e2311f19cd68c6b
Tor-Fredrik <tori.torgersen@hotmail.com>
2020-03-25 14:51:43 +0100

    modal

--- public/assets/js/gradientScript.js
+++ public/assets/js/gradientScript.js
@@ -145,32 +145,9 @@ Potree.loadPointCloud("potree/myData/pointclouds/nuPage/cloud.js", "nuPage", e =
         close3 = document.getElementById("closer3");
         close3.onclick = function(){
             el3.style.display = "none";  
+            
         }
     }
-
-    //MODAL SETUP
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementById("anno-image");
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-    }
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
-    } //end modal
- 
-
 
     material.activeAttributeName = "elevation";
     material.gradient = gradient;
@@ -191,23 +168,3 @@ Potree.loadPointCloud("potree/myData/pointclouds/nuPage/cloud.js", "nuPage", e =
 
 
 });
-
-/*
-document.getElementById('btn4').onclick = function(){
-    let anno = document.getElementsByClassName(".annotation");
-    if( anno.display === "false"){
-        console.log("1");
-        anno.display = "true";
-    } else {
-        anno.display = "false";
-        console.log("2");
-    }
-}*/
-
-
-
-
-
-
-
-
